31f9886df6279dc43643d3f52ebb95844ef71cc5
gari12jain <93127301+gari12jain@users.noreply.github.com>
2022-03-20 09:08:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MEDICAL STORE MANAGMENT SYSTEM</title>
+	<style>
+	body {
+       margin: 0px;
+       background-color: white;
+
+	}
+	.header {
+		overflow: hidden; text-align: center; background: #fff; padding-top: 60px; padding-bottom: 40px;
+	}
+	.site-name{display: inline-block;}
+.site-name h1{margin: 0; font-size: 44px; color: #34b091; font-family: 'source_sans_probold'; text-transform: uppercase; letter-spacing: 2px;}
+.site-name h2{font-size: 18px; line-height: 24px; color: #cac8c8; font-family: 'source_sans_prolight';}
+
+#navbar {
+	width: 100%;
+	overflow: auto;
+	background-color: #34b091; 
+	display: inline-block;
+	 margin-bottom: 50px;
+	 height: 52px;
+	 line-height: 50px;
+}
+#linkref
+{
+	color:white;
+	text-decoration: none;
+	padding: 15px 0px; 
+	padding-right: 40px;
+	margin-top: 5px;
+	font-size: 16px;
+}
+nav a:hover {
+    color: #888888; background: none;
+}
+.container {
+  margin: 50px auto;
+  width: 640px;
+  text-align: center;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.row {
+  margin-right: -15px;
+  margin-left: -15px;
+}
+.copy-rights{color: #34b091;font-family: 'source_sans_proregular'; background: #454855; padding: 35px 0;}
+.copy-rights a{color: #34b091;font-size: 14px; font-family: 'source_sans_proregular'; text-decoration: none;}
+.copy-rights a:hover{color: #fff;}
+
+.col-mad-6
+{
+  position: relative;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+img {
+	margin:0;
+	width:100%;
+	height:40%;
+	
+	margin-top: -54px;
+	margin-bottom: -43px;
+}
+
+    </style>
+	</head>
+	<body>
+		<div class="header">
+			<div class="site-name">
+			<h1>WELCOME TO MEDICAL STORE MANAGMENT SYSTEM</h1>
+			 <h2>welcome!!!</h2>
+		</div>
+		
+		<div id="navbar">
+			
+				<a href="index.php" id="linkref">HOME</a>
+				
+				<a href="about.php" id="linkref">ABOUT US</a>
+				
+				<a href="contact.php" id="linkref">CONTACT US</a>
+				<a href="feedback.php" id="linkref">FEEDBACK</a>
+				<a href="login.php" id="linkref">LOGIN</a>
+			
+		</div>
+		
+     <img src="images\img1.jpg"/>
+   
+
+        
+      </div>
+      <div class="copy-rights">
+      <div class="container">
+        <div class="row">
+          
+            <div class="col-md-6">
+              Project Designed by:Aayush Ranade
+              					  Garima Jain
+                                  Priyanka Patil
+                                  Rajat Rathod</a>
+            </div>
+            <div class="col-md-6">
+              A Major Project of: <a>BE 8th semester</a> / <a>SDBCT</a>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      </div>
+	</body>
+	</html>
